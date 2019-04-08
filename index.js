b6a4b68c7d9a4de88786b5155ba41e07c91f729c
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 16:52:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }
 
-var renderCommit = (commit) => {
+function renderCommit(commit) => {
   return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
 }
 
