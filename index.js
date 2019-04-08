963e95ca4a903843a55b77a7d442063df5084568
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 16:45:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 function searchRepositories() {
+  const searchTerms = $('#searchTerms').val()
   $.get(`https://api.github.com/search/repositories?q=${searchTerms}`, function(data) {
 
   }).fail(function(error) {
