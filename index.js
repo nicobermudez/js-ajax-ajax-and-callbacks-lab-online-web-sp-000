e73e0ce53df943fdad454f56d801f31799a0f3e8
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-08 16:52:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ function displayError() {
   $('#errors').html("I'm sorry, there's been an error. Please try again.")
 }
 
-function renderCommit(commit) {
+var renderCommit = (commit) => {
+  return `<li><h3>${commit.sha}</h3><p>${commit.commit.message}</p></li>`
+}
 
+var renderCommits = (data) => {
+  let result = data.map((commit)=>renderCommit(commit)).join('')
+  return `<ul>${result}</ul>`
+}
+
+var showCommits = (el) => {
+  $.get(`https://api.github.com/repos/${el.dataset.owner}/${el.dataset.repository}/commits`, data => {
+    $('#details').html(renderCommits(data))
+  }).fail(error => {
+    displayError()
+  })
 }
 
 function renderSearchResult(result) {
